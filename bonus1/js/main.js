6318821adddccfd02c6c172e39404d7dadb3f53e
Giotreglia <giovanni.treglia@gmail.com>
2023-03-01 09:48:22 +0100

    Bonus ciclo infinito funzionante

--- bonus1/js/main.js
+++ bonus1/js/main.js
@@ -44,12 +44,12 @@ let immagineCorrente = 0;
 imgContainerDom[immagineCorrente].classList.add('show');
 imgSidebarOverlayDom[immagineCorrente].classList.add('hide');
 sidebarContainerDom[immagineCorrente].classList.add('border');
-upDom.classList.add('hide');
+/* upDom.classList.add('hide'); */
 
 
 // Creo programma per cambiare immagine al clic sui pulsanti up e down
 
-// Pulsante up
+// Pulsante Down
 
 downDom.addEventListener('click',
 
@@ -66,20 +66,31 @@ downDom.addEventListener('click',
             imgSidebarOverlayDom[immagineCorrente].classList.add('hide');
             sidebarContainerDom[immagineCorrente].classList.add('border');
 
-            upDom.classList.remove('hide');
+            /* upDom.classList.remove('hide'); */
             
             if (immagineCorrente == imgContainerDom.length - 1) {
                 
-                
-                downDom.classList.add('hide');
-                
+                /* downDom.classList.add('hide'); */              
             }    
+            
+        } else if (immagineCorrente == imgContainerDom.length - 1) {
+
+            imgContainerDom[immagineCorrente].classList.remove('show');
+            imgSidebarOverlayDom[immagineCorrente].classList.remove('hide');
+            sidebarContainerDom[immagineCorrente].classList.remove('border');
+
+            immagineCorrente = 0;
+
+            imgContainerDom[immagineCorrente].classList.add('show');
+            imgSidebarOverlayDom[immagineCorrente].classList.add('hide');
+            sidebarContainerDom[immagineCorrente].classList.add('border');
             
         }
     }
 );
 
-// Pulsante down
+
+// Pulsante Up
 upDom.addEventListener('click',
 
     function() {
@@ -99,11 +110,24 @@ upDom.addEventListener('click',
             
             if (immagineCorrente == 0) {
                 
-                upDom.classList.add('hide');
+                /* upDom.classList.add('hide'); */             
                 
             }    
             
             
+        } else if (immagineCorrente == 0) {
+
+            imgContainerDom[immagineCorrente].classList.remove('show');
+            imgSidebarOverlayDom[immagineCorrente].classList.remove('hide');
+            sidebarContainerDom[immagineCorrente].classList.remove('border');
+
+
+            immagineCorrente = imgContainerDom.length -1;
+
+
+            imgContainerDom[immagineCorrente].classList.add('show');
+            imgSidebarOverlayDom[immagineCorrente].classList.add('hide');
+            sidebarContainerDom[immagineCorrente].classList.add('border');
         }
     }
 );
